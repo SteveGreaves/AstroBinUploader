aac194e5736ee8c92f077d92b8bbe091e65f98e8
Steve Greaves <sgreaves139@gmail.com>
2023-11-14 14:19:47 +0000

    New version

--- AstroBinUpload.py
+++ AstroBinUpload.py
@@ -3,164 +3,170 @@ import sys
 import pandas as pd
 from astropy.io import fits
 
-#
-#Test Change
-def extract_fits_headers(directory):
+# Suppress warnings for cleaner notebook output
+import warnings
+warnings.filterwarnings('ignore')
+
+def extract_fits_headers(directory: str) -> pd.DataFrame:
     """
-    Extracts headers from all FITS files in a specified directory and its subdirectories.
+    Extract headers from all FITS files in a specified directory and its subdirectories.
     
-    Parameters:
-    directory (str): The path of the directory containing FITS files.
+    Args:
+        directory (str): The path of the directory containing FITS files.
     
     Returns:
-    pandas.DataFrame: A DataFrame where each row represents the header of a FITS file,
-                      including the file path as an additional column.
+        pandas.DataFrame: A DataFrame where each row represents the header of a FITS file,
+                          including the file path as an additional column.
     """
     headers = []
-    for root, dirs, files in os.walk(directory):
+    for root, _, files in os.walk(directory):
         for file in files:
             if file.lower().endswith('.fits'):
                 file_path = os.path.join(root, file)
                 try:
                     with fits.open(file_path) as hdul:
                         header = hdul[0].header
-                        headers.append(dict(header, file_path=file_path))
+                        headers.append({**header, 'file_path': file_path})
                 except Exception as e:
                     print(f"Error reading {file_path}: {e}")
     return pd.DataFrame(headers)
 
-def create_calibration_df(df):
+def create_calibration_df(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Creates a DataFrame for calibration data based on specific IMAGETYPs and GAIN.
+    Create a DataFrame for calibration data based on specific IMAGETYPs and GAIN.
     
-    Parameters:
-    df (pandas.DataFrame): The DataFrame containing FITS header data.
+    Args:
+        df (pandas.DataFrame): The DataFrame containing FITS header data.
     
     Returns:
-    pandas.DataFrame: A DataFrame with columns 'TYPE', 'GAIN', and 'NUMBER'.
+        pandas.DataFrame: A DataFrame with columns 'TYPE', 'GAIN', and 'NUMBER'.
     """
-    relevant_types = ['DARK', 'BIAS', 'FLAT','FLATDARKS']
+    relevant_types = ['DARK', 'BIAS', 'FLAT', 'FLATDARKS']
     filtered_df = df[df['IMAGETYP'].isin(relevant_types)]
     group_counts = filtered_df.groupby(['IMAGETYP', 'GAIN']).size().reset_index(name='NUMBER')
     return group_counts.rename(columns={'IMAGETYP': 'TYPE'})
 
-def create_lights_df(df, calibration_df, filters_df, bortle, mean_sqm, mean_fwhm):
+
+
+def create_lights_df(df: pd.DataFrame, calibration_df: pd.DataFrame, filters_df: pd.DataFrame,
+                     bortle: int, mean_sqm: float, mean_fwhm: float) -> pd.DataFrame:
     """
-    Creates a DataFrame for 'LIGHT' type data with specified columns, replacing filter names with codes.
-    
-    Parameters:
-    df (pandas.DataFrame): The DataFrame containing FITS header data.
-    calibration_df (pandas.DataFrame): The DataFrame containing calibration data.
-    filters_df (pandas.DataFrame): The DataFrame containing filter codes.
-    bortle (int): Bortle scale value.
-    mean_sqm (float): Mean sqm value.
-    mean_fwhm (float): Mean FWHM value.
-    
+    Creates a DataFrame for 'LIGHT' type data with specified columns, 
+    replacing filter names with codes and aggregating relevant data.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing FITS header data.
+        calibration_df (pd.DataFrame): DataFrame containing calibration data.
+        filters_df (pd.DataFrame): DataFrame containing filter codes.
+        bortle (int): Bortle scale value.
+        mean_sqm (float): Mean sqm value.
+        mean_fwhm (float): Mean FWHM value.
+
     Returns:
-    pandas.DataFrame: A DataFrame with specified columns for 'LIGHT' type data.
+        pd.DataFrame: Aggregated DataFrame with 'LIGHT' type data.
     """
-    # Create a dataframe woth only light image header data extracted from the main data frame
     light_df = df[df['IMAGETYP'] == 'LIGHT'].copy()
-    # Extract the date from the date-time information
     light_df['date'] = pd.to_datetime(light_df['DATE-LOC']).dt.date
-    #group on gain
     light_df['gain'] = light_df['GAIN'].astype(calibration_df['GAIN'].dtype)
-    #rename XBINNING column
-    light_df.rename(columns={'XBINNING': 'binning'}, inplace=True)
-    
-    #Replace the filter names with Astrobin filter codes 
-    #Merge light_df with filters_df by matching the 'FILTER' column in light_df to the 'Filter' column in filters_df
-    #This combinines data from both DataFrames based on filter names.
+    light_df.rename(columns={'XBINNING': 'binning', 'EXPOSURE': 'duration'}, inplace=True)
     light_df = light_df.merge(filters_df, left_on='FILTER', right_on='filter')
 
-    #Group light_df by 'date', 'Filter code ', 'gain', and 'XBINNING'.git/
-    #then calculate the sum of all frames in the group defined on the specfic 'DATE-LOC',
-    #the mean of 'EXPOSURE', 'CCD-TEMP', and 'FOCTEMP',
-
-    aggregated = light_df.groupby(['date', 'code', 'gain', 'binning']).agg(
+    aggregated = light_df.groupby(['date', 'code', 'gain', 'binning', 'duration']).agg(
         number=('DATE-LOC', 'count'),
-        duration=('EXPOSURE', 'mean'),
-        sensorCooling=('CCD-TEMP', 'mean'),
+        sensorcooling=('CCD-TEMP', 'mean'),
         temperature=('FOCTEMP', 'mean')
     ).reset_index()
-    #aggregate all other parameters for the aggregated dataframe
-    aggregated['sensorCooling'] = aggregated['sensorCooling'].round().astype(int)
-    aggregated['flatDarks'] = 0
+
+    aggregated['sensorCooling'] = aggregated['sensorcooling'].round().astype(int)
     aggregated['bortle'] = bortle
     aggregated['meanSqm'] = mean_sqm
-    aggregated['meanFWHm'] = mean_fwhm
- 
+    aggregated['meanFwhm'] = mean_fwhm
 
-    #define function that collects all calibration frame information
-    def get_calibration_data(row, cal_type):
+    def get_calibration_data(row: pd.Series, cal_type: str) -> int:
         match = calibration_df[(calibration_df['TYPE'] == cal_type) & (calibration_df['GAIN'] == row['gain'])]
         return match['NUMBER'].sum() if not match.empty else 0
 
-    #add these to the aggregated data frame 
     aggregated['darks'] = aggregated.apply(get_calibration_data, cal_type='DARK', axis=1)
     aggregated['flats'] = aggregated.apply(get_calibration_data, cal_type='FLAT', axis=1)
     aggregated['bias'] = aggregated.apply(get_calibration_data, cal_type='BIAS', axis=1)
     aggregated['flatDarks'] = aggregated.apply(get_calibration_data, cal_type='FLATDARKS', axis=1)
     aggregated['sensorCooling'] = aggregated['sensorCooling'].round(1)
     aggregated['temperature'] = aggregated['temperature'].round(1)
-    #rename Code column
     aggregated.rename(columns={'code': 'filter'}, inplace=True)
 
-
-    #adjust the column order to match that required by AstoBin
     column_order = ['date', 'filter', 'number', 'duration', 'binning', 'gain', 
-                    'sensorCooling', 'darks', 'flats', 'flatDarks', 'bias', 
-                    'meanSqm', 'meanFWHm', 'temperature']
-    #aggregated = aggregated.reindex(columns=column_order)
+                    'sensorCooling', 'darks', 'flats', 'flatDarks', 'bias', 'bortle',
+                    'meanSqm', 'meanFwhm', 'temperature']
 
     return aggregated.reindex(columns=column_order)
 
-def process_fits_data(directory, filter_file, bortle, mean_sqm, mean_fwhm):
+
+def consolidate_headers(directories):
     """
-    Processes FITS data to create and export a CSV file with light and calibration data.
-    
-    Parameters:
-    directory (str): Directory containing FITS files.
-    filter_file (str): CSV file containing filter codes.
-    bortle (int): Bortle scale value.
-    mean_sqm (float): Mean sqm value.
-    mean_fwhm (float): Mean fwhm value.
+    Aggregates FITS file headers from multiple directories into a single pandas DataFrame.
+
+    This function iterates through a list of directories, extracts headers from FITS files in each directory using
+    the `extract_fits_headers` function, and then combines them into one DataFrame.
+
+    Args:
+        directories (list of str): A list of directory paths to process.
+
+    Returns:
+        pandas.DataFrame: A consolidated DataFrame containing all headers from the FITS files in the provided directories.
     """
-    try:
-        fits_headers_df = extract_fits_headers(directory)
-        if fits_headers_df.empty:
-            print("No FITS headers found.")
-            return
-        calibration = create_calibration_df(fits_headers_df)
-        filters = pd.read_csv(filter_file)
-        lights = create_lights_df(fits_headers_df, calibration, filters, bortle, mean_sqm, mean_fwhm)
-        output_csv = os.path.join(directory, 'AstroBin_Acquisition_File.csv')
-        lights.to_csv(output_csv, index=False)
-        print(f"Data exported to {output_csv}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    all_headers = []
+    for directory in directories:
+        headers_df = extract_fits_headers(directory)
+        all_headers.append(headers_df)
+    return pd.concat(all_headers, ignore_index=True)
 
 def main():
     """
-    Main function to prompt user input and process FITS data.
+    Main function to execute the script. 
+
+    This function performs the following steps:
+    1. Checks if the command line arguments meet the expected format (at least one directory path and three numeric arguments).
+    2. Validates the existence of the provided directory paths.
+    3. Parses and validates the last three command line arguments as numeric values.
+    4. Consolidates FITS file headers from the specified directories.
+    5. Processes and exports the 'LIGHT' type data to a CSV file.
+
+    The script exits with an error message if the command line arguments do not meet the requirements or if any of the 
+    provided directories do not exist.
     """
-    try:
-        directory = input("Enter the image directory: ")
+    # Check if there are at least four arguments (including the script name)
+    if len(sys.argv) < 4:
+        print("Error: Please provide at least one directory path and three numeric arguments.")
+        sys.exit(1)
+
+    # All arguments except the last three are directory paths
+    directory_paths = sys.argv[1:-3]
+
+    # Check if directories exist
+    for directory in directory_paths:
         if not os.path.isdir(directory):
-            raise ValueError("Directory not found.")
-        filter_file = input("Enter the filter file name: ")
-        if not os.path.isfile(filter_file):
-            raise ValueError("Filter file not found.")
-        bortle = int(input("Enter the Bortle scale value: "))
-        mean_sqm = float(input("Enter the mean SQM value: "))
-        mean_fwhm = float(input("Enter the mean FWHM value: "))
-        process_fits_data(directory, filter_file, bortle, mean_sqm, mean_fwhm)
-    except ValueError as e:
-        print(f"Invalid input: {e}")
-    except KeyboardInterrupt:
-        print("\nProcess interrupted by user.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+            print(f"Error: The directory '{directory}' does not exist.")
+            sys.exit(1)
+
+    # Try to parse the last three arguments as numeric variables
+    try:
+        numeric_variables = [float(var) for var in sys.argv[-3:]]
+    except ValueError:
+        print("Error: The last three arguments must be numeric.")
+        sys.exit(1)
+
+    # Process directories and consolidate headers
+    fits_headers_df = consolidate_headers(directory_paths)
+
+    calibration = create_calibration_df(fits_headers_df)
+    filters = pd.read_csv('filters.csv')
+    #lights = create_lights_df(fits_headers_df, calibration, filters, bortle, mean_sqm, mean_fwhm)
+    lights = create_lights_df(fits_headers_df, calibration, filters, float(sys.argv[-3]), float(sys.argv[-2]),float(sys.argv[-1]))
+
+    output_csv = os.path.basename(directory_paths[0]) + " acquisition.csv"
+    lights.to_csv(output_csv , index=False)
+    print(f"Data exported to {output_csv}")
+
 
 if __name__ == "__main__":
     main()
